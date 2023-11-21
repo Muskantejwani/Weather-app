b51b7433c107858b87546c4d59748a4aeaa76510
Muskan Tejwani <muskantejwani445@gmail.com>
2023-11-21 23:43:38 +0500

    react app

---
+++ src/api/api.js
@@ -0,0 +1,11 @@
+const apiKey = '1cd06287084f4f3d28fd2f155ead3914';
+
+const getWeather = async (city) => {
+    return await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`)
+    .then((res) => res.json())
+    .then((json) => {
+        return json;
+    })
+}
+
+export default getWeather;
